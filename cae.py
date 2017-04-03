eb50149edf8c9940fbb5da83e825816c21aec58d
rzarcone <ryanzarcone@gmail.com>
2017-04-03 13:50:26 -0700

    temp fix for queue bug; renames vars; adds dbn
    dbn is not working on deconv - possibly an issue with tensorflow
    and question-mark (unknown) dims.

--- cae.py
+++ cae.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 import tensorflow as tf
 import utils.plot_functions as pf
 import numpy as np
@@ -7,7 +7,7 @@ import numpy as np
 """Function to preprocess a single image"""
 def preprocess_image(image):
   cropped_image = tf.image.resize_image_with_crop_or_pad(image, 256, 256)
-  cropped_image = tf.to_float(cropped_image, name='ToFlaot')
+  cropped_image = tf.to_float(cropped_image, name="ToFlaot")
   cropped_image = tf.div(cropped_image, 255.0)
   ## grayscale ## 
   #cropped_image = tf.image.rgb_to_grayscale(cropped_image)
@@ -24,25 +24,62 @@ def read_image(filename_queue):
   cropped_image = preprocess_image(image)
   return cropped_image
 
-"""Make layer that does relu(conv(u,w)+b)"""
-def layer_maker(layer_num, u_in, w_shape, w_init, stride, transpose):
-  with tf.variable_scope('weights'+str(layer_num)):
-    w = tf.get_variable(name='w'+str(layer_num), shape=w_shape, dtype=tf.float32,
+"""Devisive normalizeation nonlinearity"""
+def gdn(layer_num, u_in, inverse):
+  ##TODO:
+  ##  deconv results in unknown dimensions (because img dim isn't known?)
+  ##  unknown dims are fine for normal deconv, but something in GDN is breaking it
+  ##  brian says he has done tf.multiply() and it didn't crash
+  ##  error indicates that it is happening because of the tf.stack call
+  ##  need to set up small script to test this
+  ##  Brian suggests hard coding all of the dims (in deconv and elsewhere) to see if that fixes it
+  u_in_shape = u_in.shape.dims
+  with tf.name_scope("gdn"+str(layer_num)) as scope:
+    b_gdn = tf.Variable(tf.zeros(u_in_shape[3]), trainable=True,
+      name="gdn_bias"+str(layer_num))
+    norm_weights_shape = [u_in_shape[3]]*2
+    norm_weights = tf.Variable(tf.ones(shape=norm_weights_shape,
+      dtype=tf.float32), trainable=True, name="norm_weights"+str(layer_num))
+    #print("layer_num="+str(layer_num))
+    #print(inverse)
+    #print(u_in.shape)
+    #print([u_in_shape[0]*u_in_shape[1]*u_in_shape[2], u_in_shape[3]])
+    collapsed_u_sq = tf.reshape(tf.square(u_in),
+      shape=tf.stack([u_in_shape[0]*u_in_shape[1]*u_in_shape[2], u_in_shape[3]]))
+    weighted_norm = tf.reshape(tf.matmul(collapsed_u_sq, norm_weights),
+      shape=tf.stack([u_in_shape[0], u_in_shape[1], u_in_shape[2], u_in_shape[3]]))
+    GDN_const = tf.sqrt(tf.add(b_gdn, weighted_norm))
+    if inverse:
+      u_out = tf.multiply(u_in, GDN_const, name="u_gdn"+str(layer_num))
+    else:
+      u_out = tf.divide(u_in, GDN_const, name="u_gdn"+str(layer_num))
+    print(u_out.shape)
+  return u_out
+
+"""
+Make layer that does activation(conv(u,w)+b)
+  where activation() is either relu or GDN
+"""
+def layer_maker(layer_num, u_in, w_shape, w_init, stride, decode, relu, god_damn_network):
+  #print("layer_maker")
+  #print(u_in.shape)
+  with tf.variable_scope("weights"+str(layer_num)):
+    w = tf.get_variable(name="w"+str(layer_num), shape=w_shape, dtype=tf.float32,
       initializer=w_init, trainable=True)
 
-  with tf.name_scope('biases'+str(layer_num)) as scope:                                                             
-    if not transpose:
-      b = tf.Variable(tf.zeros(w_shape[3]), trainable=True,
+  with tf.name_scope("biases"+str(layer_num)) as scope:
+    if decode:
+      b = tf.Variable(tf.zeros(w_shape[2]), trainable=True,
         name="latent_bias"+str(layer_num)) 
     else:
-      b = tf.Variable(tf.zeros(w_shape[2]), trainable=True,
+      b = tf.Variable(tf.zeros(w_shape[3]), trainable=True,
         name="latent_bias"+str(layer_num)) 
 
   with tf.name_scope("hidden"+str(layer_num)) as scope:
-    if not transpose:
-      u_out = tf.nn.relu(tf.add(tf.nn.conv2d(u_in, w, [1, stride, stride, 1], 
-        padding="SAME", use_cudnn_on_gpu=True), b), name="activation"+str(layer_num))
-    else:
+    if decode:
+      if god_damn_network:
+        u_in = gdn(layer_num, u_in, decode)
+      #print(u_in.shape)
       height_const = tf.where(tf.equal(tf.mod(tf.shape(u_in)[1], stride), 0), 0, 1)
       out_height = tf.shape(u_in)[1] * stride - height_const
       width_const = tf.where(tf.equal(tf.mod(tf.shape(u_in)[2], stride), 0), 0, 1)
@@ -51,34 +88,60 @@ def layer_maker(layer_num, u_in, w_shape, w_init, stride, transpose):
         tf.cast(out_height, tf.int32), # Height
         tf.cast(out_width, tf.int32), # Width
         tf.constant(w_shape[2], dtype=tf.int32)]) # Channels
-      u_out = tf.nn.relu(tf.add(tf.nn.conv2d_transpose(u_in, w, out_shape,
-        strides=[1, stride, stride, 1], padding="SAME"), b),
+      u_out = tf.add(tf.nn.conv2d_transpose(u_in, w, out_shape,
+        strides=[1, stride, stride, 1], padding="SAME"), b,
         name="activation"+str(layer_num))
-
+      #print(u_out.shape)
+      if relu:
+        u_out = tf.nn.relu(u_out, name="relu_activation")
+    else:
+      u_out = tf.add(tf.nn.conv2d(u_in, w, [1, stride, stride, 1],
+        padding="SAME", use_cudnn_on_gpu=True), b, name="activation"+str(layer_num))
+      #print(u_out.shape)
+      if relu:
+        u_out = tf.nn.relu(u_out, name="relu_activation")
+      if god_damn_network:
+        u_out = gdn(layer_num, u_out, decode)
   return w, b, u_out
+
+"""Print shapes of all u in u_list"""
+def u_print(u_list):
+  u_print_str = ""
+  for idx, u in enumerate(u_list):
+   u_eval = sess.run(u)
+   u_shape = u_eval.shape
+   num_u = u_eval.size
+   u_print_str+="\tu"+str(idx)+"_shape: "+str(u_shape)+"\t"+str(num_u)+"\n"
+   print(u_print_str)
 
 #general params
 file_location = "/home/dpaiton/Work/Datasets/imagenet/imgs.txt"
 num_gpus = 1
 num_threads = 5
-num_epochs = 1
+num_epochs = 10
 seed = 1234567890
 
 #image params
 shuffle = True
-num_epochs = 1
 batch_size = 100
 img_shape_y = 256
 img_shape_x = 256
 num_colors = 3
+batches_per_epoch = 450
+
+#learning rates
+init_learning_rate = 1e-5
+decay_steps = 0.5*batch_size*batches_per_epoch
+staircase = True
+decay_rate = 0.8 # for ADADELTA
 
 #layer params
-learning_rate = 1e-8
-decay_rate = 0.95 # for ADADELTA
-input_channels = [num_colors]#[num_colors, 128, 128]
-output_channels = [128]#[128, 128, 128]
-patch_size_y = [9]#[9, 5, 5]
-strides = [2]#[4, 2, 2]
+god_damn_network = False
+relu = False
+input_channels = [num_colors,192,192]#[num_colors, 128, 128]
+output_channels = [192,192,192]#[128, 128, 128]
+patch_size_y = [9,5,5]#[9, 5, 5]
+strides = [4,2,2]#[4, 2, 2]
 
 #queue params
 patch_size_x = patch_size_y
@@ -101,7 +164,7 @@ capacity = min_after_dequeue + (num_read_threads + 1) * batch_size
 graph = tf.Graph()
 with graph.as_default():
   with tf.name_scope("queue") as scope:
-    # Make a list of filenames. Strip removes '\n' at the end
+    # Make a list of filenames. Strip removes "\n" at the end
     # file_location contains image locations separated by newlines (piped from ls)
     filenames = tf.constant([string.strip()
       for string
@@ -138,9 +201,9 @@ with graph.as_default():
   w_inits += w_inits # decode inits are the same as encode inits
  
   for layer_idx, vals in enumerate(zip(w_shapes, strides)):
-    transpose = False if layer_idx < num_layers/2 else True
+    decode = False if layer_idx < num_layers/2 else True
     w, b, u_out = layer_maker(layer_idx, u_list[layer_idx], vals[0],
-      w_inits[layer_idx], vals[1], transpose)
+      w_inits[layer_idx], vals[1], decode, relu, god_damn_network)
     w_list.append(w)
     u_list.append(u_out)
     b_list.append(b)
@@ -150,9 +213,17 @@ with graph.as_default():
       reduction_indices=[1,2,3]))
 
   with tf.name_scope("optimizers") as scope:
+    learning_rates = tf.train.exponential_decay(
+      learning_rate=init_learning_rate,
+      global_step=global_step,
+      decay_steps=decay_steps,
+      decay_rate=decay_rate,
+      staircase=staircase,
+      name="annealing_schedule")
     #optimizer = tf.train.AdadeltaOptimizer(learning_rate, rho=decay_rate, name="grad_optimizer")
     train_vars = w_list + b_list
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate, name="grad_optimizer")
+    optimizer = tf.train.AdamOptimizer(learning_rates, name="grad_optimizer")
+    #optimizer = tf.train.GradientDescentOptimizer(learning_rate, name="grad_optimizer")
     grads_and_vars = optimizer.compute_gradients(total_loss, var_list=train_vars)
     train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
@@ -161,14 +232,14 @@ with graph.as_default():
     pSNRdB = tf.multiply(10.0, tf.log(tf.div(tf.pow(1.0, 2.0), MSE)), name="recon_quality")
 
   with tf.name_scope("summaries") as scope:
-    tf.summary.image('input', u_list[0]) 
-    tf.summary.image('reconstruction',u_list[-1])
-    [tf.summary.histogram('u'+str(idx),u) for idx,u in enumerate(u_list)]
-    [tf.summary.histogram('w'+str(idx),w) for idx,w in enumerate(w_list)]
-    [tf.summary.histogram('b'+str(idx),b) for idx,b in enumerate(b_list)]
-    tf.summary.scalar('total_loss', total_loss)
-    tf.summary.scalar('MSE', MSE)
-    tf.summary.scalar('pSNRdB', pSNRdB)
+    tf.summary.image("input", u_list[0])
+    tf.summary.image("reconstruction",u_list[-1])
+    [tf.summary.histogram("u"+str(idx),u) for idx,u in enumerate(u_list)]
+    [tf.summary.histogram("w"+str(idx),w) for idx,w in enumerate(w_list)]
+    [tf.summary.histogram("b"+str(idx),b) for idx,b in enumerate(b_list)]
+    tf.summary.scalar("total_loss", total_loss)
+    tf.summary.scalar("MSE", MSE)
+    tf.summary.scalar("pSNRdB", pSNRdB)
 
   gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
   config = tf.ConfigProto()
@@ -177,54 +248,39 @@ with graph.as_default():
   # Must initialize local variables as well as global to init num_epochs
   # in tf.train.string_input_produce
   merged = tf.summary.merge_all()
-  train_writer = tf.summary.FileWriter('/home/rzarcone/CAE_Project/CAEs' + '/train', graph)
+  train_writer = tf.summary.FileWriter("/home/rzarcone/CAE_Project/CAEs" + "/train", graph)
   init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
 with tf.Session(config=config, graph=graph) as sess:
   sess.run(init_op)
   # Coordinator manages threads, checks for stopping requests
-  coord = tf.train.Coordinator()
-  # Both start_queue_runners and create_threads must be called to enqueue the images
-  # TODO: Why do we need to do both of these? the start=True flag on create_threads should cover
-  #        what start_queue_runners does.
-  _ = tf.train.start_queue_runners(sess, coord, start=True)
-  enqueue_threads = qr.create_threads(sess, coord=coord, start=True)
-
-  try:
-    with coord.stop_on_exception():
-      while not coord.should_stop():
-        #w_shapes = [sess.run(w).shape for w in w_list]
-        #u_shapes = [sess.run(u).shape for u in u_list]
-        #b_shapes = [sess.run(b).shape for b in b_list]
-        #import IPython; IPython.embed(); raise SystemExit
-        sess.run(train_op)
-        step = sess.run(global_step)
-        if step % 10 == 0:
-          ### SUMMARY ##
-          summary = sess.run(merged)
-          train_writer.add_summary(summary, step)
-          weight_filename = "/home/rzarcone/CAE_Project/CAEs/train/weights/"
-          w_enc_eval = np.squeeze(sess.run(tf.transpose(w_list[0], perm=[3,0,1,2]))) 
-          pf.save_data_tiled(w_enc_eval, normalize=True, title="Weights0",
-            save_filename=weight_filename+"Weights_enc.png")
-          w_dec_eval = np.squeeze(sess.run(tf.transpose(w_list[-1], perm=[3,0,1,2]))) 
-          pf.save_data_tiled(w_dec_eval, normalize=True, title="Weights-1",
-            save_filename=weight_filename+"Weights_dec.png")
-          ## Print stuff
-          eval_total_loss = sess.run(total_loss)
-          snr = sess.run(pSNRdB)
-          print("step %g\t\tloss %g\tpSNRdB %g"%(step, eval_total_loss, snr))
-          #u_print_str = ""
-          #for idx, u in enumerate(u_list):
-          #  u_eval = sess.run(u)
-          #  u_shape = u_eval.shape
-          #  num_u = u_eval.size
-          #  u_print_str+="\tu"+str(idx)+"_shape: "+str(u_shape)+"\t"+str(num_u)+"\n"
-          #print(u_print_str)
-        if step == 400:
-          coord.request_stop()
-  except tf.errors.OutOfRangeError:
-    print("Done training -- epoch limit reached")
-  finally:
-    coord.request_stop()
+  for n in range(num_epochs):
+    coord = tf.train.Coordinator()
+    # Both start_queue_runners and create_threads must be called to enqueue the images
+    # TODO: Why do we need to do both of these? the start=True flag on create_threads should cover
+    #        what start_queue_runners does.
+    #       Hard-coded batches_per_epoch must be less than the actual number of available batches
+    #        how to allow the queue to totally empty before refilling?
+    _ = tf.train.start_queue_runners(sess, coord, start=True)
+    enqueue_threads = qr.create_threads(sess, coord=coord, start=True)
+    for i in range(batches_per_epoch):
+      sess.run(train_op)
+      step = sess.run(global_step)
+      if step % 10 == 0:
+        ### SUMMARY ##
+        summary = sess.run(merged)
+        train_writer.add_summary(summary, step)
+        weight_filename = "/home/rzarcone/CAE_Project/CAEs/train/weights/"
+        w_enc_eval = np.squeeze(sess.run(tf.transpose(w_list[0], perm=[3,0,1,2])))
+        pf.save_data_tiled(w_enc_eval, normalize=True, title="Weights0",
+          save_filename=weight_filename+"Weights_enc.png")
+        w_dec_eval = np.squeeze(sess.run(tf.transpose(w_list[-1], perm=[3,0,1,2])))
+        pf.save_data_tiled(w_dec_eval, normalize=True, title="Weights-1",
+          save_filename=weight_filename+"Weights_dec.png")
+        ## Print stuff
+        eval_total_loss = sess.run(total_loss)
+        snr = sess.run(pSNRdB)
+        print("step %g\t\tloss %g\tpSNRdB %g"%(step, eval_total_loss, snr))
+        #u_print(u_list)
+  coord.request_stop()
   coord.join(enqueue_threads)
