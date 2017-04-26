14e13edb57f1dba2eb32a9a7c7f88112c0f071d5
rzarcone <ryanzarcone@gmail.com>
2017-04-25 19:10:36 -0700

    Added ability to work across multiple GPUs

--- cae.py
+++ cae.py
@@ -13,7 +13,7 @@ def preprocess_image(image):
   cropped_image = tf.div(cropped_image, 255.0)
   cropped_image = tf.subtract(cropped_image, tf.reduce_mean(cropped_image))
   ## grayscale ## 
-  #cropped_image = tf.image.rgb_to_grayscale(cropped_image)
+  cropped_image = tf.image.rgb_to_grayscale(cropped_image)
   return cropped_image
 
 """Function to load in a single image"""
@@ -28,10 +28,10 @@ def read_image(filename_queue):
   return cropped_image
 
 def memristorize(u_in, memristor_std_eps):
-  with tf.name_scope("memristor_transform") as scope:
+  with tf.variable_scope("memristor_transform") as scope:
     path = 'data/Partial_Reset_PCM.pkl'
     #n_mem = tf.reduce_prod(u_out.get_shape()[1:])
-    n_mem = 49152
+    n_mem = 49152 # 49152
     (vs_data, mus_data, sigs_data,
       orig_VMIN, orig_VMAX, orig_RMIN,
       orig_RMAX) = get_data.get_memristor_data(path, n_mem, num_ext=5,
@@ -46,16 +46,17 @@ def memristorize(u_in, memristor_std_eps):
 """Devisive normalizeation nonlinearity"""
 def gdn(layer_num, u_in, inverse):
   u_in_shape = u_in.get_shape()
-  with tf.name_scope("gdn"+str(layer_num)) as scope:
+  with tf.variable_scope("gdn"+str(layer_num)) as scope:
     #small_const = tf.multiply(tf.ones(u_in_shape[3]),2e-5)
     small_const = tf.multiply(tf.ones(u_in_shape[3]),1e-3)
-    b_gdn = tf.Variable(tf.add(tf.zeros(u_in_shape[3]), small_const), 
-      trainable=True, dtype=tf.float32,  name="gdn_bias"+str(layer_num))
+    b_gdn = tf.get_variable(name="gdn_bias"+str(layer_num), dtype=tf.float32,
+      initializer=tf.add(tf.zeros(u_in_shape[3]), small_const), trainable=True)
     b_threshold = tf.where(tf.less(b_gdn, tf.constant(1e-3, dtype=tf.float32)),
       tf.multiply(tf.ones_like(b_gdn), 1e-3), b_gdn)
     w_gdn_shape = [u_in_shape[3]]*2
     w_gdn_init = tf.multiply(tf.ones(shape=w_gdn_shape, dtype=tf.float32), 1e-3)
-    w_gdn = tf.Variable(w_gdn_init, trainable=True, name="w_gdn"+str(layer_num))
+    w_gdn = tf.get_variable(name="w_gdn"+str(layer_num), dtype=tf.float32,
+      initializer=w_gdn_init, trainable=True)
     w_threshold = tf.where(tf.less(w_gdn, tf.constant(1e-3, dtype=tf.float32)), w_gdn_init, w_gdn)
     collapsed_u_sq = tf.reshape(tf.square(u_in),
       shape=tf.stack([u_in_shape[0]*u_in_shape[1]*u_in_shape[2], u_in_shape[3]]))
@@ -81,15 +82,15 @@ def layer_maker(layer_num, u_in, w_shape, w_init, stride, decode, relu, god_damn
     w = tf.get_variable(name="w"+str(layer_num), shape=w_shape, dtype=tf.float32,
       initializer=w_init, trainable=True)
 
-  with tf.name_scope("biases"+str(layer_num)) as scope:
+  with tf.variable_scope("biases"+str(layer_num)) as scope:
     if decode:
-      b = tf.Variable(tf.zeros(w_shape[2]), trainable=True,
-        name="latent_bias"+str(layer_num)) 
+      b = tf.get_variable(name="latent_bias"+str(layer_num), dtype=tf.float32,
+        initializer=tf.zeros(w_shape[2]), trainable=True) 
     else:
-      b = tf.Variable(tf.zeros(w_shape[3]), trainable=True,
-        name="latent_bias"+str(layer_num)) 
+      b = tf.get_variable(name="latent_bias"+str(layer_num), dtype=tf.float32,
+        initializer=tf.zeros(w_shape[3]), trainable=True) 
 
-  with tf.name_scope("hidden"+str(layer_num)) as scope:
+  with tf.variable_scope("hidden"+str(layer_num)) as scope:
     if decode:
       if god_damn_network:
         u_in, b_gdn, w_gdn = gdn(layer_num, u_in, decode)
@@ -115,6 +116,29 @@ def layer_maker(layer_num, u_in, w_shape, w_init, stride, decode, relu, god_damn
         u_out, b_gdn, w_gdn = gdn(layer_num, u_out, decode)
   return w, b, u_out, b_gdn, w_gdn
 
+"""
+Average gradients computed per GPU
+Args:
+ grad_list: list of lists of (gradint, variable) tuples.
+   Outer list is per tower, inner list is per varaible
+"""
+def average_gradients(grad_list):
+  avg_grads = []
+  for grad_and_vars in zip(*grad_list):
+    # star unpacks outer loop, such that each grad_and_vars
+    # looks like the following:
+    #   iter 0: ((grad0_gpu0, var0_gpu0), .., (grad0_gpuN, var0_gpuN))
+    #   iter 1: ((grad1_gpu0, var1_gpu0), .., (grad1_gpuN, var1_gpuN))
+    grads = []
+    for g, _ in grad_and_vars:
+      # Add 0 dimension to the gradients to represent the tower
+      expanded_g = tf.expand_dims(g, 0)
+      grads.append(expanded_g)
+    grad = tf.reduce_mean(tf.concat(axis=0, values=grads), axis=0)
+    v = grad_and_vars[0][1] # the variables are shared across towers
+    avg_grads.append((grad, v))
+  return avg_grads
+
 """Print shapes of all u in u_list"""
 def u_print(u_list):
   u_print_str = ""
@@ -127,19 +151,18 @@ def u_print(u_list):
 
 #general params
 file_location = "/home/dpaiton/Work/Datasets/imagenet/imgs.txt"
-num_gpus = 1
-gpu_id = "/gpu:0"
+gpu_ids = ["0", "1"]
 num_threads = 5
-num_epochs = 20
+num_epochs = 1
 seed = 1234567890
 
 #image params
-shuffle = True
-batch_size = 50
+shuffle_inputs = True
+batch_size = 25
 img_shape_y = 256
 img_shape_x = 256
-num_colors = 3
-batches_per_epoch = 900 #450
+num_colors = 1
+batches_per_epoch = 10#900 #450
 
 #learning rates
 init_learning_rate = 5e-4
@@ -151,15 +174,16 @@ decay_rate = 0.9 # for ADADELTA
 memristorify = True
 god_damn_network = True
 relu = False
-input_channels = [num_colors,192,192]#[num_colors, 128, 128]
-output_channels = [192,192,192]#[128, 128, 128]
-patch_size_y = [9,5,5]#[9, 5, 5]
-strides = [4,2,2]#[4, 2, 2]
+input_channels = [num_colors,192,192,192]#[num_colors, 128, 128]
+output_channels = [192,192,192,192]#[128, 128, 128]
+patch_size_y = [3,3,5,5]#[9, 5, 5]
+strides = [2,2,2,2]#[4, 2, 2]
 GAMMA = 1.0  # slope of the out of bounds cost
 mem_v_min = -1.0
 mem_v_max = 1.0
 
 #queue params
+num_gpus = len(gpu_ids) 
 patch_size_x = patch_size_y
 w_shapes = [vals for vals in zip(patch_size_y, patch_size_x, input_channels,
   output_channels)]
@@ -170,127 +194,139 @@ input_channels += input_channels[::-1]
 output_channels += output_channels[::-1]
 strides += strides[::-1]
 num_layers = len(w_shapes)
-
-min_after_dequeue = 10
-
+min_after_dequeue = 20
 im_shape = [img_shape_y, img_shape_x, num_colors]
 num_read_threads = num_threads * num_gpus
 capacity = min_after_dequeue + (num_read_threads + 1) * batch_size
 
 graph = tf.Graph()
-with tf.device(gpu_id):
-  with graph.as_default():
-    with tf.name_scope("queue") as scope:
-      # Make a list of filenames. Strip removes "\n" at the end
-      # file_location contains image locations separated by newlines (piped from ls)
-      filenames = tf.constant([string.strip()
-        for string
-        in open(file_location, "r").readlines()])
+with graph.as_default(),tf.device('/cpu:0'):
+  with tf.name_scope("step_counter") as scope:
+    global_step = tf.Variable(0, trainable=False, name="global_step")
 
-      # Turn list of filenames into a string producer to feed names for each thread
-      # Shuffling happens here - should be faster than shuffling after the images are loaded
-      # Capacity is the max capacity of the queue - can be adjusted as needed
-      filename_queue = tf.train.string_input_producer(filenames, num_epochs, shuffle=shuffle,
-        seed=seed, capacity=capacity)
+  with tf.variable_scope("optimizers") as scope:
+    learning_rates = tf.train.exponential_decay(
+      learning_rate=init_learning_rate,
+      global_step=global_step,
+      decay_steps=decay_steps,
+      decay_rate=decay_rate,
+      staircase=staircase,
+      name="annealing_schedule")
+    optimizer = tf.train.AdamOptimizer(learning_rates, name="grad_optimizer")
 
-      # FIFO queue requires that all images have the same dtype & shape
-      queue = tf.FIFOQueue(capacity, dtypes=[tf.float32], shapes=im_shape)
-      # Enqueues one element at a time
-      enqueue_op = queue.enqueue(read_image(filename_queue)) 
+  with tf.variable_scope("placeholders") as scope:
+    #n_mem = tf.reduce_prod(u_in.get_shape()[1:])
+    n_mem = 49152 # 49152
+    memristor_std_eps = tf.placeholder(tf.float32, shape=(num_gpus*batch_size, n_mem))
 
-    with tf.name_scope("input") as scope:
-      # Reads a batch of images from the queue
-      x = queue.dequeue_many(batch_size) # Requires that all images are the same shape
 
-    with tf.name_scope("queue") as scope:
-      # Holds a list of enqueue operations for a queue, each to be run in a thread.
-      qr = tf.train.QueueRunner(queue, [enqueue_op] * num_read_threads)
+  with tf.variable_scope("queue") as scope:
+    # Make a list of filenames. Strip removes "\n" at the end
+    # file_location contains image locations separated by newlines (piped from ls)
+    filenames = tf.constant([string.strip()
+      for string
+      in open(file_location, "r").readlines()])
 
-    with tf.name_scope("step_counter") as scope:
-      global_step = tf.Variable(0, trainable=False, name="global_step")
+    # Turn list of filenames into a string producer to feed names for each thread
+    # Shuffling happens here - should be faster than shuffling after the images are loaded
+    # Capacity is the max capacity of the queue - can be adjusted as needed
+    filename_queue = tf.train.string_input_producer(filenames, num_epochs,
+      shuffle=shuffle_inputs, seed=seed, capacity=capacity)
 
-    w_list = []
-    u_list = [x]
-    b_list = []
-    b_gdn_list = []
-    w_gdn_list = []
+    # FIFO queue requires that all images have the same dtype & shape
+    queue = tf.FIFOQueue(capacity, dtypes=[tf.float32], shapes=im_shape)
+    # Enqueues one element at a time
+    enqueue_op = queue.enqueue(read_image(filename_queue)) 
 
-    w_inits = [tf.contrib.layers.xavier_initializer_conv2d(uniform=False, seed=seed,
-      dtype=tf.float32) for _ in np.arange(num_layers/2)]
-    w_inits += w_inits # decode inits are the same as encode inits
-   
-    for layer_idx, w_shapes_strides in enumerate(zip(w_shapes, strides)):
-      decode = False if layer_idx < num_layers/2 else True
-      w, b, u_out, b_gdn, w_gdn = layer_maker(layer_idx, u_list[layer_idx], w_shapes_strides[0],
-        w_inits[layer_idx], w_shapes_strides[1], decode, relu, god_damn_network)
-      if memristorify:
-        if layer_idx == num_layers/2-1:
+  with tf.variable_scope("queue") as scope:
+    # Holds a list of enqueue operations for a queue, each to be run in a thread.
+    qr = tf.train.QueueRunner(queue, [enqueue_op] * num_read_threads)
+
+  with tf.variable_scope("input") as scope:
+    # Reads a batch of images from the queue
+    x = queue.dequeue_many(batch_size) # Requires that all images are the same shape
+
+  gradient_list = []
+  # tf.get_variable_scope().reuse_variables() call will only apply
+  # to items within this with statement
+  with tf.variable_scope(tf.get_variable_scope()):
+    for gpu_id in gpu_ids:
+      with tf.device('/gpu:'+gpu_id):
+        with tf.name_scope("tower_"+gpu_id) as scope:
+          w_list = []
+          u_list = [x]
+          b_list = []
+          b_gdn_list = []
+          w_gdn_list = []
+          w_inits = [tf.contrib.layers.xavier_initializer_conv2d(uniform=False, seed=seed,
+            dtype=tf.float32) for _ in np.arange(num_layers/2)]
+          w_inits += w_inits # decode inits are the same as encode inits
+          for layer_idx, w_shapes_strides in enumerate(zip(w_shapes, strides)):
+            decode = False if layer_idx < num_layers/2 else True
+            w, b, u_out, b_gdn, w_gdn = layer_maker(layer_idx, u_list[layer_idx], w_shapes_strides[0],
+              w_inits[layer_idx], w_shapes_strides[1], decode, relu, god_damn_network)
+            if memristorify:
+              if layer_idx == num_layers/2-1:
+                with tf.variable_scope("loss") as scope:
+                  # Penalty for going out of bounds
+                  reg_loss = tf.reduce_mean(tf.reduce_sum(GAMMA * (tf.nn.relu(u_out- mem_v_max)
+                    + tf.nn.relu(mem_v_min - u_out)), axis=[1,2,3]))
+                memristor_std_eps_slice = tf.split(value=memristor_std_eps,
+                  num_or_size_splits=num_gpus, axis=0)[int(gpu_id)]
+                u_out = memristorize(u_out, memristor_std_eps_slice)
+            w_list.append(w)
+            u_list.append(u_out)
+            b_list.append(b)
+            b_gdn_list.append(b_gdn)
+            w_gdn_list.append(w_gdn)
+
           with tf.variable_scope("loss") as scope:
-            # Penalty for going out of bounds
-            reg_loss = tf.reduce_mean(tf.reduce_sum(GAMMA * (tf.nn.relu(u_out- mem_v_max)
-              + tf.nn.relu(mem_v_min - u_out)), axis=[1,2,3]))
-          with tf.name_scope("placeholders") as scope:
-            #n_mem = tf.reduce_prod(u_out.get_shape()[1:])
-            n_mem = 49152
-            memristor_std_eps = tf.placeholder(tf.float32, shape=(u_out.get_shape()[0], n_mem))
-          u_out = memristorize(u_out, memristor_std_eps)
-      w_list.append(w)
-      u_list.append(u_out)
-      b_list.append(b)
-      b_gdn_list.append(b_gdn)
-      w_gdn_list.append(w_gdn)
+            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.pow(tf.subtract(u_list[0], u_list[-1]), 2.0),
+              axis=[1,2,3]))
+            total_loss = tf.add_n([recon_loss, reg_loss], name="total_loss")
 
-    with tf.variable_scope("loss") as scope:
-      recon_loss = tf.reduce_mean(tf.reduce_sum(tf.pow(tf.subtract(u_list[0], u_list[-1]), 2.0),
-        axis=[1,2,3]))
-      total_loss = tf.add_n([recon_loss, reg_loss], name="total_loss")
+          with tf.variable_scope("optimizers") as scope:
+            train_vars = w_list + b_list
+            train_vars += b_gdn_list + w_gdn_list if god_damn_network else []
+            grads_and_vars = optimizer.compute_gradients(total_loss, var_list=train_vars)
+            gradient_list.append(grads_and_vars)
 
-    with tf.name_scope("optimizers") as scope:
-      learning_rates = tf.train.exponential_decay(
-        learning_rate=init_learning_rate,
-        global_step=global_step,
-        decay_steps=decay_steps,
-        decay_rate=decay_rate,
-        staircase=staircase,
-        name="annealing_schedule")
-      #optimizer = tf.train.AdadeltaOptimizer(learning_rate, rho=decay_rate, name="grad_optimizer")
-      train_vars = w_list + b_list
-      train_vars += b_gdn_list + w_gdn_list if god_damn_network else []
-      optimizer = tf.train.AdamOptimizer(learning_rates, name="grad_optimizer")
-      #optimizer = tf.train.GradientDescentOptimizer(learning_rates, name="grad_optimizer")
-      grads_and_vars = optimizer.compute_gradients(total_loss, var_list=train_vars)
-      train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+          tf.get_variable_scope().reuse_variables()
 
-    with tf.name_scope("performance_metrics") as scope:
-      MSE = tf.reduce_mean(tf.square(tf.subtract(u_list[0],
-        tf.clip_by_value(u_list[-1], clip_value_min=-1.0, clip_value_max=1.0))),
-        name="mean_squared_error")
-      SNRdB = tf.multiply(10.0, tf.log(tf.div(tf.square(tf.nn.moments(u_list[0], axes=[0,1,2,3])[1]), MSE)), name="recon_quality")
+  avg_grads = average_gradients(gradient_list)
+  with tf.variable_scope("optimizers") as scope:
+    train_op = optimizer.apply_gradients(avg_grads, global_step=global_step)
 
-    with tf.name_scope("summaries") as scope:
-      tf.summary.image("input", u_list[0])
-      tf.summary.image("reconstruction",u_list[-1])
-      [tf.summary.histogram("u"+str(idx),u) for idx,u in enumerate(u_list)]
-      [tf.summary.histogram("w"+str(idx),w) for idx,w in enumerate(w_list)]
-      [tf.summary.histogram("b"+str(idx),b) for idx,b in enumerate(b_list)]
-      [tf.summary.histogram("b_gdn"+str(idx),u) for idx,u in enumerate(b_gdn_list)]
-      [tf.summary.histogram("w_gdn"+str(idx),w) for idx,w in enumerate(w_gdn_list)]
-      tf.summary.scalar("total_loss", total_loss)
-      tf.summary.scalar("MSE", MSE)
-      tf.summary.scalar("SNRdB", SNRdB)
+  with tf.name_scope("performance_metrics") as scope:
+    MSE = tf.reduce_mean(tf.square(tf.subtract(u_list[0],
+      tf.clip_by_value(u_list[-1], clip_value_min=-1.0, clip_value_max=1.0))),
+      name="mean_squared_error")
+    SNRdB = tf.multiply(10.0, tf.log(tf.div(tf.square(tf.nn.moments(u_list[0], axes=[0,1,2,3])[1]), MSE)), name="recon_quality")
 
-    #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-    config = tf.ConfigProto()
-    #config.gpu_options.per_process_gpu_memory_fraction=0.5
-    config.gpu_options.allow_growth = True
-    config.log_device_placement = False # for debugging - log devices used by each variable
+  with tf.name_scope("summaries") as scope:
+    tf.summary.image("input", u_list[0])
+    tf.summary.image("reconstruction",u_list[-1])
+    [tf.summary.histogram("u"+str(idx),u) for idx,u in enumerate(u_list)]
+    [tf.summary.histogram("w"+str(idx),w) for idx,w in enumerate(w_list)]
+    [tf.summary.histogram("b"+str(idx),b) for idx,b in enumerate(b_list)]
+    [tf.summary.histogram("b_gdn"+str(idx),u) for idx,u in enumerate(b_gdn_list)]
+    [tf.summary.histogram("w_gdn"+str(idx),w) for idx,w in enumerate(w_gdn_list)]
+    tf.summary.scalar("total_loss", total_loss)
+    tf.summary.scalar("MSE", MSE)
+    tf.summary.scalar("SNRdB", SNRdB)
 
-    # Must initialize local variables as well as global to init num_epochs
-    # in tf.train.string_input_produce
-    merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter("/home/rzarcone/CAE_Project/CAEs" + "/train", graph)
-    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+  # Must initialize local variables as well as global to init num_epochs
+  # in tf.train.string_input_produce
+  merged = tf.summary.merge_all()
+  train_writer = tf.summary.FileWriter("/home/rzarcone/CAE_Project/CAEs" + "/train", graph)
+  init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+config = tf.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction=0.5
+config.gpu_options.allow_growth = True
+config.allow_soft_placement = True
+config.log_device_placement = False # for debugging - log devices used by each variable
 with tf.Session(config=config, graph=graph) as sess:
   sess.run(init_op)
   # Coordinator manages threads, checks for stopping requests
@@ -305,12 +341,12 @@ with tf.Session(config=config, graph=graph) as sess:
     enqueue_threads = qr.create_threads(sess, coord=coord, start=True)
     for i in range(batches_per_epoch):
       #n_mem_eval = sess.run(tf.to_int32(tf.size(u_list[int(num_layers/2)])/u_list[int(num_layers/2)].get_shape()[0]))
-      n_mem_eval = 49152
-      mem_std_eps = np.random.standard_normal((batch_size, n_mem_eval)).astype(np.float32)
+      n_mem_eval =49152 # 49152
+      mem_std_eps = np.random.standard_normal((num_gpus*batch_size, n_mem_eval)).astype(np.float32)
       feed_dict={memristor_std_eps:mem_std_eps}
       sess.run(train_op, feed_dict=feed_dict)
-      step = sess.run(global_step)
-      if step % 10 == 0:
+      step = sess.run(global_step, feed_dict=feed_dict)
+      if step % 1 == 0:
         ### SUMMARY ##
         summary = sess.run(merged, feed_dict=feed_dict)
         train_writer.add_summary(summary, step)
@@ -325,8 +361,8 @@ with tf.Session(config=config, graph=graph) as sess:
         ## Print stuff
         [ev_reg_loss, ev_recon_loss, ev_total_loss] = sess.run([reg_loss, recon_loss, total_loss],
           feed_dict=feed_dict)
-        snr = sess.run(SNRdB, feed_dict=feed_dict)
-        print("step %04d\treg_loss %03g\trecon_loss %g\ttotal_loss %g\tSNRdB %g"%(
+        snr = sess.run(MSE, feed_dict=feed_dict)
+        print("step %04d\treg_loss %03g\trecon_loss %g\ttotal_loss %g\tMSE %g"%(
           step, ev_reg_loss, ev_recon_loss, ev_total_loss, snr))
         #u_print(u_list)
   coord.request_stop()
