6ea73dea07ee849408ea0972b3b704b2d83283ff
Dylan <dpaiton@gmail.com>
2017-10-25 16:41:49 -0700

    setup eval stuff

--- eval_cae.py
+++ eval_cae.py
@@ -5,35 +5,39 @@ import os
 import csv
 import tensorflow as tf
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
-import utils.plot_functions as pf
 from cae_model import cae
+import DeepSparseCoding.utils.image_processing as ip
+import DeepSparseCoding.utils.plot_functions as pf
 
 params = {}
 #shitty hard coding
-params["n_mem"] = 32768#3072 for max, 7680 for med, 32768 for min
+params["n_mem"] = 32768#32768#3072 for max, 7680 for med, 32768 for min
 
 #checkpoints
 
 #params["check_load_run_name"] = "3072_max_compress_pcm" #ep49_56300
 #params["check_load_run_name"] = "7680_med_compress_pcm" #ep39_45040
-#params["check_load_run_name"] = "32768_min_compress_pcm" #ep49-56300
+params["check_load_run_name"] = "32768_min_compress_pcm" #ep49-56300
 
 #params["check_load_run_name"] = "3072_max_compress_gauss" #ep49_56300
 #params["check_load_run_name"] = "7680_med_compress_gauss" #ep49-56300
-params["check_load_run_name"] = "32768_min_compress_gauss" #ep49-56300
+#params["check_load_run_name"] = "32768_min_compress_gauss" #ep49-56300
 
 #params["check_load_run_name"] = "3072_max_compress_pcm_relu" #ep49_56300 # GDN3 Not Found
 #params["check_load_run_name"] = "7680_med_compress_pcm_relu" #ep49-56300
 
 #general params
 params["run_name"] = "eval_"+params["check_load_run_name"]
-params["file_location"] = "/media/tbell/datasets/kodak/image_list.txt"
+#params["file_location"] = "/media/tbell/datasets/kodak/image_list.txt"
+params["file_location"] = "/media/tbell/datasets/test_images.txt"
 params["gpu_ids"] = ["0"]
 params["output_location"] = os.path.expanduser("~")+"/CAE_Project/CAEs/model_outputs/"+params["run_name"]
 params["num_threads"] = 1
 params["num_epochs"] = 1
-params["epoch_size"] = 24
+#params["epoch_size"] = 24
+params["epoch_size"] = 49900
 params["eval_interval"] = 1
 params["seed"] = 1234567890
 params["check_load_path"] = "/home/dpaiton/CAE_Project/CAEs/model_outputs/"+params["check_load_run_name"]+"/checkpoints/chkpt_ep49-56300"
@@ -78,6 +82,7 @@ config.gpu_options.allow_growth = True
 config.allow_soft_placement = True
 config.log_device_placement = False # for debugging - log devices used by each variable
 
+#TODO: set up for-loop over batches to agg correlation matrix, save corr matrix, load in ipynb
 with tf.Session(config=config, graph=cae_model.graph) as sess:
   sess.run(cae_model.init_op)
   cae_model.full_saver.restore(sess, cae_model.params["check_load_path"])
@@ -87,13 +92,20 @@ with tf.Session(config=config, graph=cae_model.graph) as sess:
   # set memristor noise
   mem_std_eps = np.random.standard_normal((cae_model.params["effective_batch_size"],
     cae_model.params["n_mem"])).astype(np.float32)
-  feed_dict={cae_model.memristor_std_eps:mem_std_eps}
+  feed_dict = {cae_model.memristor_std_eps:mem_std_eps}
   tf_var_list = cae_model.train_vars + cae_model.u_list
   loss_list = [cae_model.total_loss, cae_model.reg_loss, cae_model.recon_loss]
   eval_list = tf_var_list + loss_list + [cae_model.MSE, cae_model.SNRdB]
   out_vars = sess.run(eval_list, feed_dict=feed_dict)
 
-  #import IPython; IPython.embed(); raise SystemExit
+  ulist = sess.run(cae_model.u_list, feed_dict)
+  pickle.dump(ulist, open("/home/dpaiton/CAE_Project/"+params["check_load_run_name"]+"_ulist.npz", "wb"))
+  #np.savez("/home/dpaiton/CAE_Project/"+params["check_load_run_name"]+"_ulist.npz", data=ulist)
+
+  weights = sess.run(cae_model.train_vars, feed_dict)
+  np.savez("/home/dpaiton/CAE_Project/"+params["check_load_run_name"]+"_weights.npz", data=weights)
+
+  import IPython; IPython.embed(); raise SystemExit
 
   num_img_pixels = cae_model.params["img_shape_y"]*cae_model.params["img_shape_x"]*cae_model.params["num_colors"]
   mem_per_pixel = cae_model.params["n_mem"]/num_img_pixels
